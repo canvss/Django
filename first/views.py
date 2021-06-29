f94b814fb2f3e2a97dbe15fdb77ebe4155b9f447
endless <endliss@sina.cn>
2021-06-30 01:36:39 +0800

    Learn Django The first day of learning Django

---
+++ first/views.py
@@ -0,0 +1,20 @@
+from random import sample
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import  HttpResponse
+def show_index(request):
+    fruits =[
+        'Apple','Orange','Pitaya','Durian','Mango','Pear','Peach','Grape'
+    ]
+
+    selected_fruits = sample(fruits,3)
+    #第一个参数request对象
+    #第二个参数返回需要渲染静态网页名字
+    #第三个返回需要渲染到页面数据
+    return render(request,'index.html',{'fruits':selected_fruits})
+
+
+def show_weather(request):
+    pass
